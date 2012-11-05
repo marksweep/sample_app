becaf537061abe59ea5f572262241dd11949c0c9
David Wu <david@marksweep.com>
2012-11-05 13:11:59 -0500

    Adds micro posts to show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
 	attr_accessible :email, :name, :password, :password_confirmation
 	has_secure_password
 
+	has_many :microposts, dependent: :destroy
+
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
