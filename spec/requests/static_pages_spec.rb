5fa5d1d668706f94a4cdc0b7a1fea15a44347799
David Wu <david@marksweep.com>
2012-11-06 05:04:15 +0100

    Adds relationships for follower and following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,76 +2,87 @@ require_relative '../spec_helper'
 
 describe "Static pages" do
 
-    subject { page }
+		subject { page }
 
-    shared_examples_for "all static pages" do
-        it { should have_selector( 'h1',    text: heading) }
-        it { should have_selector('title',      text: full_title(page_title)) }
-    end
+		shared_examples_for "all static pages" do
+				it { should have_selector( 'h1',    text: heading) }
+				it { should have_selector('title',      text: full_title(page_title)) }
+		end
 
-  describe "Home page" do
-    before { visit root_path }
-    let(:heading) { 'Sample App' }
-    let(:page_title) { '' }
+	describe "Home page" do
+		before { visit root_path }
+		let(:heading) { 'Sample App' }
+		let(:page_title) { '' }
 
-    it_should_behave_like "all static pages"
-    it { should_not have_selector 'title', text: '| Home' }
+		it_should_behave_like "all static pages"
+		it { should_not have_selector 'title', text: '| Home' }
 
-    describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-        sign_in user
-        visit root_path
-      end
+		describe "for signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+				sign_in user
+				visit root_path
+			end
 
-      it "should render a users feed" do
-        user.feed.each do |item|
-          page.should have_selector("li##{item.id}", text: item.content)
-        end
-      end
-    end
-  end
+			it "should render a users feed" do
+				user.feed.each do |item|
+					page.should have_selector("li##{item.id}", text: item.content)
+				end
+			end
 
-  describe "Help page" do
-    before { visit help_path }
-    let(:heading) { 'Help' }
-    let(:page_title) { 'Help' }
+			describe "follower / following counts" do
+				let(:other_user) { FactoryGirl.create(:user) }
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
 
-    it_should_behave_like "all static pages"
-  end
+				it { should have_link("0 following"), href: following_user_path(user) }
+				it { should have_link("1 followers"), href: followers_user_path(user) }
+			end
+		end
+	end
 
-  describe "About page" do
-    before { visit about_path }
+	describe "Help page" do
+		before { visit help_path }
+		let(:heading) { 'Help' }
+		let(:page_title) { 'Help' }
 
-    let(:heading) { 'About Us' }
-    let(:page_title) { 'About Us' }
+		it_should_behave_like "all static pages"
+	end
 
-    it_should_behave_like "all static pages"
-  end
+	describe "About page" do
+		before { visit about_path }
 
-  describe "Contact page" do
-    before { visit contact_path }
+		let(:heading) { 'About Us' }
+		let(:page_title) { 'About Us' }
 
-    let(:heading) { 'Contact' }
-    let(:page_title) { 'Contact' }
+		it_should_behave_like "all static pages"
+	end
 
-    it_should_behave_like "all static pages"
-  end
+	describe "Contact page" do
+		before { visit contact_path }
 
-  it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    page.should have_selector 'title', text:full_title('About Us')
-    click_link "Help"
-    page.should have_selector 'title', text:full_title('Help')
-    click_link "Contact"
-    page.should have_selector 'title', text:full_title('Contact')
-    click_link "Home"
-    click_link "Sign up now!"
-    page.should have_selector 'title', text:full_title('Sign up')
-    click_link "sample_app"
-    page.should have_selector 'title', text:full_title('')
+		let(:heading) { 'Contact' }
+		let(:page_title) { 'Contact' }
+
+		it_should_behave_like "all static pages"
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		page.should have_selector 'title', text:full_title('About Us')
+		click_link "Help"
+		page.should have_selector 'title', text:full_title('Help')
+		click_link "Contact"
+		page.should have_selector 'title', text:full_title('Contact')
+		click_link "Home"
+		click_link "Sign up now!"
+		page.should have_selector 'title', text:full_title('Sign up')
+		click_link "sample_app"
+		page.should have_selector 'title', text:full_title('')
 end
 end
